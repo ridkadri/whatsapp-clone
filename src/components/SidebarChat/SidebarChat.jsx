b4825a114d36fafc5a01a12c8c076ce98de46128
Ridwan <kadriridwan0@gmail.com>
2020-12-06 22:00:29 +0000

    created overall styling and sidebar component

---
+++ src/components/SidebarChat/SidebarChat.jsx
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react';
+import {Avatar} from '@material-ui/core';
+import './SidebarChat.css';
+
+const SidebarChat = (addNewChat) => {
+    const [seed, setSeed] = useState('');
+
+    useEffect(()=> {
+        setSeed(Math.floor(Math.random() * 5000));
+    },[]);
+
+    const createChat = () => {
+        const roomName = prompt('Please enter name for chat');
+
+        if(roomName) {
+            //do some stuff in database
+        }
+    }
+
+    return !addNewChat ? (
+    <div className='sidebarChat'>
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+        <div className='sidebarChat_info'>
+            <h2>Room Name</h2>
+            <p>Last message...</p>
+        </div>
+    </div>
+    
+    ): (
+        <div onClick={createChat} className='sidebarChat'>
+            <h2>Add New Chat</h2>
+        </div>
+    );
+}
+
+export default SidebarChat;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCiW16wHVvn8qJTDU3n6csaUgQGMogXmBg",
+    authDomain: "whatsapp-clone-5b206.firebaseapp.com",
+    projectId: "whatsapp-clone-5b206",
+    storageBucket: "whatsapp-clone-5b206.appspot.com",
+    messagingSenderId: "61174835472",
+    appId: "1:61174835472:web:805f93a56cfac4d7a45d95"
+  };
